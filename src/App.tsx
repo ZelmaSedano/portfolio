4ece6d7974766ece21d2dd9526ca3571549e494b
ZelmaSedano <zvsedano@gmail.com>
2025-03-01 09:47:41 -0600

    adding filler text to activate scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
 
   return (
     <>
+      <div className="scrollable-content">
         <header>
           <section className='blue-bar'>Zelma Sedano</section>
           <nav className='navbar'>
@@ -15,8 +16,17 @@ function App() {
           </nav>
         </header>
         <div className='content'>
-          
+            <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Placerat tempor mollis quisque nullam euismod odio tincidunt et nec. Himenaeos curae mattis rhoncus mus rhoncus lobortis rutrum vulputate. Neque curae efficitur at lobortis lobortis mollis. Praesent ad efficitur eleifend pulvinar faucibus cubilia efficitur elementum. Hendrerit ipsum ultrices natoque integer lectus fames fringilla nec. Pretium condimentum felis nullam inceptos ultricies adipiscing ad nunc felis. Quam ornare nunc phasellus rutrum hac sem. Tempus semper mauris eros tincidunt ac purus.
+
+            Cras nam primis fermentum leo neque potenti, faucibus orci consequat. Pulvinar condimentum neque aptent torquent cubilia. Nec facilisis aptent vestibulum ac; netus dis eget consectetur. Efficitur inceptos nisi orci convallis elit fusce, consectetur augue. Condimentum viverra potenti lectus sed taciti maecenas mus elit. Integer velit lacus lobortis tortor nam per laoreet. Suspendisse velit magnis libero tellus fames sapien. Per magna ultricies nunc; vulputate molestie in.
+
+            Ridiculus at etiam suscipit conubia tempor. Netus non metus tellus purus molestie leo luctus. Mauris nisi interdum, amet lacus lectus natoque vivamus semper. Cursus cras vestibulum pretium penatibus netus in pellentesque platea? Litora habitant neque facilisi erat odio condimentum laoreet risus blandit. Fames elit volutpat eros mauris tempus rutrum, arcu dui integer. Ullamcorper tristique integer auctor integer aliquet velit luctus mauris. Eros dolor mi potenti blandit mollis montes auctor quam.
+
+            Interdum faucibus rhoncus varius justo nisl nunc at mauris metus. Facilisis eleifend conubia etiam lectus netus tempor molestie dignissim. Dolor mi phasellus tortor laoreet sed nisi ultrices. Facilisi ridiculus etiam eu arcu habitant sociosqu. Torquent consectetur venenatis consectetur mollis posuere natoque morbi urna. Quam eros mi justo orci est faucibus netus dignissim.
+
+            Fames congue primis fringilla purus velit proin porttitor erat. Senectus integer facilisi ultricies eget rhoncus leo; tristique scelerisque a. Parturient tellus volutpat sed urna, interdum mi eros. Cursus ad diam gravida vulputate nisl duis consequat vivamus purus. Ridiculus urna congue a bibendum sit inceptos id ornare. Nisl risus mattis porta nunc elementum cubilia lacinia. Tempor hac ultrices facilisi eget faucibus metus fames himenaeos. Donec venenatis volutpat velit sapien pharetra consectetur. Nibh vel pharetra maximus auctor congue etiam per elit.</p>
         </div>
+      </div>
     </>
   )
 }

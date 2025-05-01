8d9e69e3edc884301df0e5c5101c06f20176cab6
ZelmaSedano <zvsedano@gmail.com>
2025-04-30 21:05:05 -0500

    making desktop window

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           </nav>
         </header>
         <div className='content'>
-            <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Placerat tempor mollis quisque nullam euismod odio tincidunt et nec. Himenaeos curae mattis rhoncus mus rhoncus lobortis rutrum vulputate. Neque curae efficitur at lobortis lobortis mollis. Praesent ad efficitur eleifend pulvinar faucibus cubilia efficitur elementum. Hendrerit ipsum ultrices natoque integer lectus fames fringilla nec. Pretium condimentum felis nullam inceptos ultricies adipiscing ad nunc felis. Quam ornare nunc phasellus rutrum hac sem. Tempus semper mauris eros tincidunt ac purus.
+            <p className='paragraph'>Lorem ipsum odor amet, consectetuer adipiscing elit. Placerat tempor mollis quisque nullam euismod odio tincidunt et nec. Himenaeos curae mattis rhoncus mus rhoncus lobortis rutrum vulputate. Neque curae efficitur at lobortis lobortis mollis. Praesent ad efficitur eleifend pulvinar faucibus cubilia efficitur elementum. Hendrerit ipsum ultrices natoque integer lectus fames fringilla nec. Pretium condimentum felis nullam inceptos ultricies adipiscing ad nunc felis. Quam ornare nunc phasellus rutrum hac sem. Tempus semper mauris eros tincidunt ac purus.
 
             Cras nam primis fermentum leo neque potenti, faucibus orci consequat. Pulvinar condimentum neque aptent torquent cubilia. Nec facilisis aptent vestibulum ac; netus dis eget consectetur. Efficitur inceptos nisi orci convallis elit fusce, consectetur augue. Condimentum viverra potenti lectus sed taciti maecenas mus elit. Integer velit lacus lobortis tortor nam per laoreet. Suspendisse velit magnis libero tellus fames sapien. Per magna ultricies nunc; vulputate molestie in.
 

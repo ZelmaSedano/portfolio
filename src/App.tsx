5dc56eee79e485753afb291023fdb4a54fe7815a
ZelmaSedano <zvsedano@gmail.com>
2025-05-14 09:03:26 -0500

    adding footer section and earth icon

--- src/App.tsx
+++ src/App.tsx
@@ -40,14 +40,29 @@ function App() {
 
         <div className='content'>
           <p className='banner'>Welcome to my lil corner of the internet!</p>
+
+          {/* BIO SECTION */}
           <div className='bio-section'>
             <img src="/src/assets/mee.jpg" className='bio-image'></img>
 
             <p className='bio-p'>This site is my homage to vintage web design. </p>
           </div>
 
+          {/* CONTENT FOOTER */}
+          {/* use the footer section for the border styling */}
+          <div className="footer">
+            <div className='footer-section footer-large'></div>
+            <div className = 'footer-section footer-small'></div>
+            <div className = 'footer-section footer-small'></div>
+            <div className = 'footer-section footer-small'></div>
+            <div className='footer-section footer-medium'>
+              <img src="/src/assets/earth.ico" className='content-footer-icon'></img>
+              <p className='footer-section-text'>Internet</p>
+            </div>
+          </div>
+
         </div>
-        {/* ^ content section*/}
+        {/* end content section*/}
 
       </div>
     </>

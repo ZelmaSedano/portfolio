5bf9f350edbe806937e115297a21ee3264162a9a
ZelmaSedano <zvsedano@gmail.com>
2025-07-29 12:35:47 -0500

    adding state, updating start menu to include an actual menu, and a click-away event handler func

--- src/components/Taskbar.tsx
+++ src/components/Taskbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 interface TaskbarProps {
     isVisible: boolean;
@@ -7,12 +7,58 @@ interface TaskbarProps {
 }
 
 const Taskbar: React.FC<TaskbarProps> = ({isVisible, toggleWindow, currentTime}) => {
+    // states 
+    // is menu open?
+    const [isStartMenuOpen, setIsStartMenuOpen] = useState(false);
+    // useRefs can access DOM nodes directly
+    const startMenuRef = useRef<HTMLDivElement>(null);
+    const startButtonRef = useRef<HTMLButtonElement>(null);
+
+    // close menu when clicking outside
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (
+                startMenuRef.current && 
+                !startMenuRef.current.contains(event.target as Node) &&
+                startButtonRef.current &&
+                !startButtonRef.current.contains(event.target as Node)
+            ) {
+                setIsStartMenuOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    // event handlers
+    const toggleStartMenu = () => {
+        setIsStartMenuOpen(!isStartMenuOpen);
+    };
+
+
     return (
         <div className="taskbar">
-            <button className="start-button">
+            <button 
+                ref={startButtonRef}
+                className={`start-button ${isStartMenuOpen ? 'active' : ''}`}
+                onClick={toggleStartMenu}
+            >
                 <img src="/src/assets/flag.png" className="start-icon" alt="start"/>
                 <span className="start-text">Start</span>
             </button>
+
+            {isStartMenuOpen && (
+                <div ref={startMenuRef} className="start-menu">
+                    <div className="start-menu-items">
+                        <div className="start-menu-item"><span className='underline'>L</span>inkedIn</div>
+                        <div className="start-menu-item"><span className='underline'>G</span>itHub</div>
+                        <div className="start-menu-item"><span className='underline'>W</span>ebCraftian Labs</div>
+                    </div>
+                </div>
+            )}
 
             <div className='devscape-section'>
                 <button 

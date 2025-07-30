2ef344f5869acc48ae58b86b3edbf77269d6e661
ZelmaSedano <zvsedano@gmail.com>
2025-07-30 11:02:29 -0500

    added className and imgClassName as option so I could set custom image styling for horoscope icon

--- src/components/DesktopIcon.tsx
+++ src/components/DesktopIcon.tsx
@@ -6,17 +6,19 @@ interface DesktopIconProps {
     x: number;
     y: number;
     onClick: () => void;
+    className?: string;
+    imgClassName?: string;
 }
 
 // try and make this the standard, too many varying react function declarations imho 
-const DesktopIcon: React.FC<DesktopIconProps> = ({icon, label, x, y, onClick}) => {
+const DesktopIcon: React.FC<DesktopIconProps> = ({icon, label, x, y, onClick, className='', imgClassName = '',}) => {
     return (
         <div
-        className="desktop-icon"
+        className={`desktop-icon ${className}`}
         style={{ position: 'absolute', left: x, top: y }}
         onClick={onClick}
         >
-        <img src={icon} alt={label} />
+        <img src={icon} alt={label} className={imgClassName}/>
         <span>{label}</span>
         </div>
     );

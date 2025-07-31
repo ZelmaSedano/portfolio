950b70acd9994cda8692660e69d11d1167d79084
ZelmaSedano <zvsedano@gmail.com>
2025-07-31 07:07:53 -0500

    updating so 3rd party API calls work

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// VITE WAS BLOCKING THE DAMN API CALL
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      }
+    }
+  }
 })

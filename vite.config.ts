f8e82b91edb378502cd95f8a3b97eeaa377a7392
ZelmaSedano <zvsedano@gmail.com>
2025-07-31 08:29:22 -0500

    adding some comments about why vite blocks api calls and returns html instead

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // VITE WAS BLOCKING THE DAMN API CALL
+
+/* here's why it wasn't working:
+  - react app makes request to /api/horoscope?sign=aries
+  - vite's dev server intercepts because it starts with /api
+  - VITE PASSES THIS RESPONSE AS A HTML RESPONSE instead of JSON
+*/
 export default defineConfig({
   plugins: [react()],
   server: {

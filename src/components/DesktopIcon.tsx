07d5c93b949de7c28b795756bb89ae2f9378985e
ZelmaSedano <zvsedano@gmail.com>
2025-08-07 16:00:55 -0500

    adding active styling so icons are highlighted when selected

--- src/components/DesktopIcon.tsx
+++ src/components/DesktopIcon.tsx
@@ -1,3 +1,4 @@
+import {useState, useEffect} from 'react';
 import './DesktopIcon.css';
 
 interface DesktopIconProps {
@@ -12,11 +13,33 @@ interface DesktopIconProps {
 
 // try and make this the standard, too many varying react function declarations imho 
 const DesktopIcon: React.FC<DesktopIconProps> = ({icon, label, x, y, onClick, className='', imgClassName = '',}) => {
+    // state
+    const [isActive, setIsActive] = useState(false);
+
+    useEffect(() => {
+        const handleDocumentClick = (e: MouseEvent) => {
+            if (!(e.target as Element).closest('.desktop-icon')) {
+                setIsActive(false);
+            }
+        };
+
+        document.addEventListener('click', handleDocumentClick);
+        return () => document.removeEventListener('click', handleDocumentClick);
+    }, []);
+
+    // pass in onClick() so the parent component b/c the onClick inside the DesktopIcon now isn't just onClick, but handleClick
+    const handleClick = (e: React.MouseEvent) => {
+        // keeps from de-selecting
+        e.stopPropagation();
+        setIsActive(true);
+        onClick();
+    };
+
     return (
         <div
-        className={`desktop-icon ${className}`}
+        className={`desktop-icon ${isActive ? 'active' : ''} ${className}`}
         style={{ position: 'absolute', left: x, top: y }}
-        onClick={onClick}
+        onClick={handleClick}
         >
         <img src={icon} alt={label} className={imgClassName}/>
         <span>{label}</span>

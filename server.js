11726e1323f205b766f51feb7abd6c143b5a2bfd
ZelmaSedano <zvsedano@gmail.com>
2025-12-11 10:36:32 -0600

    moving server to 3002 so I can run 3001 at the same time

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('*', (req, res) => {
 
 // For local development
 if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 3001;
+  const PORT = process.env.PORT || 3002;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
